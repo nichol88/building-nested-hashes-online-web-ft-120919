eb3bc1cf7fb402386788e26bf2dcc0a9e724c663
Brian Nicholls <nichol88@gmail.com>
2019-12-11 00:28:34 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -46,7 +46,17 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+          }, {
+            :name => "Nurse",
+            :age => "44",
+            :attitude => "worried"
+          }
+      ]
    }
   }
 
